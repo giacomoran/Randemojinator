ddf94d969cb5abfd9d3e64f312c80cb69542b0a8
Giacomo Randazzo <giacomoran@gmail.com>
2015-07-30 23:08:57 +0200

    Added share buttons.
    Links are not working yet.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -135,6 +135,19 @@
         }
     }
 
+    //share button
+    $('.share').on('click', function(e) {
+
+		e.preventDefault();
+
+		var $this = $(this);
+
+		$this.animate({
+			'width': $this.width() == 304 ? '64px' : '304px'
+		}, 300, 'swing');
+
+	});
+
 
 
     // var vm = this;
