b72f55812ff2ec9e8f1203e7da3b121ac04ea67e
Giacomo Randazzo <giacomoran@gmail.com>
2015-08-12 23:27:55 +0200

    Finished about page up.

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,6 +13,10 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('about', {
+          url: '/about',
+          templateUrl: 'app/main/about.html'
       });
 
     $urlRouterProvider.otherwise('/');
